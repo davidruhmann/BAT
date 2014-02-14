d6d837d0a2075ab7f1c7fb2ea8a58bc911ea5295
David Ruhmann <davidruhmann@gmail.com>
2014-02-14 08:54:32 -0600

    Update SO21736349891976.bat

--- SO21736349891976.bat
+++ SO21736349891976.bat
@@ -2,33 +2,84 @@
 :: For http://stackoverflow.com/a/21736349/891976
 
 :: Example
-@echo on
+@echo off
 :: Declare Outside Scope
 setlocal enabledelayedexpansion
+echo Outside
+set "e=5"
+echo(a=!a!
+echo(b=!b!
+echo(c=!c!
+echo(d=!d!
+echo(e=!e!
 :: Declare Inside Scope
 setlocal enabledelayedexpansion
 :: Build Inside Variables
+echo.
 echo Inside
 set "a=hi@%%$<>()[]|*~';,./?:{}+_-=\/ & bye"
 set "b=2"
 set "c=^^no ^& """g"o"
 set "d=hello^!"
+set "e="
 :: View Variables Inside Scope
 echo(a=!a!
 echo(b=!b!
 echo(c=!c!
 echo(d=!d!
+echo(e=!e!
 :: Return A C and D
+::call :return2 a c d e
+::pause
+echo return a c d e
 call :return a c d e
 ::set return
 endlocal %return%
 :: View Variables Outside Scope
+echo.
 echo Outside
 echo(a=!a!
 echo(b=!b!
 echo(c=!c!
 echo(d=!d!
+echo(e=!e!
 endlocal
+pause
+exit /b 0
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Escape the special characters within a variable value.
+:: Parameter variable may not be equal to the internal variable name "_" underscore.
+:escape <variable> <outvar>
+setlocal enabledelayedexpansion
+set "_=%~2"
+if not defined _ endlocal & exit /b 1
+if not defined %~2 endlocal & exit /b 2
+set "%~2="
+set "_=%~1"
+if not defined _ endlocal & exit /b 3
+if not defined %~1 endlocal & exit /b 4
+set "_=!%~1!"
+set "_=%_:"=""%"
+set "_=%_:^=^^%"
+set "_=%_:<=^<%"
+set "_=%_:>=^>%"
+set "_=%_:&=^&%"
+set "_=%_:|=^|%"
+setlocal disabledelayedexpansion
+set "_=%_:!=^^^^^^^!%"
+endlocal & endlocal & set "%~2=%_:""="%"
+exit /b 0
+
+
+:return2 [Variables...]
+set "return="
+setlocal enabledelayedexpansion
+::for %%A in (%*) do for /f "tokens=1,* delims==" %%B in ('set %%~A') do if /i "%%~A"=="%%~B" for /f "tokens=1,* delims==" %%D ('set return') do if /i "return"=="%%~D" set "return=%%E&set "%%~A=%%C""
+::for %%A in (%*) do call :escape %%~A value && set return=!return!^&set "%%~A=!value!"
+set return
+endlocal & %return%
 exit /b 0
 
 
@@ -50,8 +101,8 @@ set "%~1=!%~1:<=^<!"
 set "%~1=!%~1:>=^>!"
 set "%~1=!%~1:&=^&!"
 set "%~1=!%~1:|=^|!"
-set "return=!return!^&set ""%~1=!%~1!"""
 :__return
+set "return=!return!^&set ""%~1=!%~1!"""
 if not "%~2"=="" shift & goto _return
 setlocal disabledelayedexpansion
 set "return=%return:!=^^^^^^^!%"
