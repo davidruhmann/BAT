d2c64ba29c4f21ed9fea480c6d8731463c6575c9
David Ruhmann <davidruhmann@gmail.com>
2016-03-29 11:16:35 -0400

    Update JenkinsVCBuild.bat

--- JenkinsVCBuild.bat
+++ JenkinsVCBuild.bat
@@ -2,10 +2,10 @@
 call :Main
 exit /b
 
-:: VCBuild (2015-01-23)
+:: VCBuild (2016-01-06)
 :: Visual Studio and Klocwork Build Manager
 ::
-:: Copyright (c) 2014 David Ruhmann
+:: Copyright (c) 2015-2016 David Ruhmann
 ::
 :: Permission is hereby granted, free of charge, to any person obtaining a copy
 :: of this software and associated documentation files (the "Software"), to deal
@@ -31,10 +31,12 @@ rem Requires Klocwork and an SVN client be in the PATH environment variable.
 rem TODO VCRegister for C# CAS might require reading the csproj for dependencies
 rem TODO Finish Windows Mobile support
 rem TODO Add Comments to Scripts and more Logging
+rem TODO Add Branch Isolation Option
 
 
 :Main
 setlocal
+call :Init
 call :GitAdjust
 call :LoadProject || goto _MainEnd
 call :ParseLabels
@@ -42,18 +44,21 @@ call :DetectBranch
 call :DefaultValues
 call :Execute || goto _MainEnd
 :_MainEnd
-call :LogErrors
+call :LogDeferredErrors
 @echo on & @endlocal & @exit /b %LastError%
 
 
-:Build <Configuration> <Version> <Verbosity> <Code Analysis> <Rule Set> [Klocwork] {KLOCWORK} {PROJECT}
+:Build <Configuration> <Version> <Verbosity> <Code Analysis> <Rule Set> [Klocwork|HPFortify] {HPFORTIFY} {KLOCWORK} {PROJECT}
 setlocal
 call :Define VCBIN %1
 set "Debug=" & if /i "%~1"=="Debug" set "Debug=YES"
 :: If Klocwork enabled, but just building Debug regular without code analysis, exit to prevent building Debug twice.
 call :IsTrue Debug && call :IsTrue Klocwork && call :Not :IsSharp %PROJECT% && call :Not :Defined %6 && call :Not :EqualsTrue %4 && (endlocal & exit /b 0)
+:: If HPFortify enabled, but just building Debug regular without code analysis, exit to prevent building Debug twice.
+call :IsTrue Debug && call :IsTrue HPFortify && call :Not :IsSharp %PROJECT% && call :Not :Defined %6 && call :Not :EqualsTrue %4 && (endlocal & exit /b 0)
 call :BuildEnvironment %Debug%
 call :TidyEnvironment
+call :VCClean %1
 call :Registration
 if "%~2"=="90" call :VCBuild %1 %6
 if "%~2"=="100" call :MSBuild %1 %2 %3 %4 %5 %6
@@ -65,18 +70,18 @@ endlocal & exit /b %ErrorLevel%
 :: setup the build environment list variables from the system
 :BuildEnvironment [Debug Flag] {INCLUDE} {LIB} {LIBPATH} {PATH} {DPATH} {VCINCLUDE} {VCINCLUDED} {VCLIB} {VCLIBD} {VCREFERENCE} {VCREFERENCED} {VCPATH} {VCPATHD}
 :: Update the Environment Variables
-call :Define INCLUDE "%VCINCLUDE%;%INCLUDE%;"
-call :Define LIB "%VCLIB%;%LIB%;"
-call :Define LIBPATH "%VCREFERENCE%;%LIBPATH%;"
-call :Define PATH "%VCPATH%;%PATH%;"
-call :Define DPATH "%PATH%;"
+call :Define INCLUDE "%VCINCLUDE:"=%;%INCLUDE:"=%;"
+call :Define LIB "%VCLIB:"=%;%LIB:"=%;"
+call :Define LIBPATH "%VCREFERENCE:"=%;%LIBPATH:"=%;"
+call :Define PATH "%VCPATH:"=%;%PATH:"=%;"
+call :Define DPATH "%PATH:"=%;"
 call :Defined %1 || exit /b 0
 :: Using Debug Environment Variables
-call :Define INCLUDE "%VCINCLUDED%;%INCLUDE%;"
-call :Define LIB "%VCLIBD%;%LIB%;"
-call :Define LIBPATH "%VCREFERENCED%;%LIBPATH%;"
-call :Define PATH "%VCPATHD%;%PATH%;"
-call :Define DPATH "%PATH%;"
+::call :Define INCLUDE "%INCLUDE:"=%;"
+call :Define LIB "%VCLIBD:"=%;%LIB:"=%;"
+call :Define LIBPATH "%VCREFERENCED:"=%;%LIBPATH:"=%;"
+call :Define PATH "%VCPATHD:"=%;%PATH:"=%;"
+call :Define DPATH "%PATH:"=%;"
 exit /b 0 : {INCLUDE} {LIB} {LIBPATH} {PATH} {DPATH}
 
 
@@ -91,8 +96,11 @@ call :Define VCLOG %VCLOG% n
 call :Define VCCAN %VCCAN% NO
 call :Define VCCAR %VCCAR% NativeRecommendedRules.ruleset
 if not defined VCCON set "VCCON=Release;Debug"
+call :Define HPFORTIFY %HPFORTIFY% %LABEL_HPFORTIFY% NO
+call :Define PVSSTUDIO %PVSSTUDIO% %LABEL_PVSSTUDIO% NO
 call :Define KLOCWORK %KLOCWORK% %LABEL_KLOCWORK% NO
 call :Define KWIMPORT %KWIMPORT%
+call :Define VCCACHE %VCCACHE% YES
 rem Update the INPUT and OUTPUT paths with the VCVER and PIPELINE values
 call :ParseRepository
 call :Define INPUT "%VCINPUT%"
@@ -115,13 +123,20 @@ echo [INFO] Millennium Mobile Detected
 call :Expand VCINCLUDE "%VCINCLUDE_NET%"
 call :Expand VCLIB "%VCLIB_NET%"
 call :Expand VCLIBD "%VCLIBD_NET%"
-exit /b 0 {VCINCLUDE} {VCLIB} {VCLIBD}
+exit /b 0 : {VCINCLUDE} {VCLIB} {VCLIBD}
 
 
 :Execute {VCCON} {VCVER} {VCARC} {VCLOG} {VCCAN} {VCCAR} {PROJECT} {KWIMPORT}
 call :VCVars %VCVER% %VCARC% exit /b 1
 for %%A in ("%VCCON:;=" "%") do call :DeferError :Build %%A %VCVER% %VCLOG% %VCCAN% %VCCAR%
+set "HPFortifyLast=%HPFortify%"
+set "HPFortify=false"
 call :DeferError :Klocwork Debug %VCVER% %VCLOG% %PROJECT% %KWIMPORT%
+set "HPFortify=%HPFortifyLast%"
+set "KlocworkLast=%Klocwork%"
+set "Klocwork=false"
+call :DeferError :HPFortify Debug %VCVER% %VCLOG% %PROJECT%
+set "Klocwork=%KlocworkLast%"
 exit /b %LastError%
 
 
@@ -159,7 +174,7 @@ exit /b %ErrorLevel%
 
 :GitAdjust ~IO
 call :GitSparse || exit /b 1
-pushd "%GIT_SPARSE%" && set "WORKSPACE=%CD%"
+pushd "%GIT_SPARSE%" && set "WORKSPACE=%CD%\%GIT_SPARSE%"
 exit /b %ErrorLevel% : {WORKSPACE} {GIT_SPARSE}
 
 
@@ -170,6 +185,28 @@ if not defined GIT_SPARSE exit /b 1
 set "GIT_SPARSE=%GIT_SPARSE:/=\%"
 echo [INFO] Git sparse point found
 exit /b 0 : {GIT_SPARSE}
+
+
+:HPFortify <Configuration> <Version> <Verbosity> {HPFortify} {PROJECT_NAME}
+sourceanalyzer -b %PROJECT_NAME% -clean --project-root .fortify
+sourceanalyzer -b %PROJECT_NAME% -clean
+call :IsTrue HPFortify || exit /b 0
+echo [INFO] Running HPFortify Build...
+call :Build %1 %2 %3 NO null "sourceanalyzer -b %PROJECT_NAME%" || exit /b 1
+echo [INFO] HPFortify Build succeeded
+echo [INFO] Running HPFortify Analysis...
+::call cloudscan.bat -url %FORTIFY_CLOUDCONTROLLER_URL% start -b %PROJECT_NAME% -scan -block -o -f Scan.fpr
+sourceanalyzer -b %PROJECT_NAME% -scan -f Scan.fpr
+echo [INFO] HPFortify Analysis succeeded
+sourceanalyzer -b %PROJECT_NAME% -clean --project-root .fortify
+sourceanalyzer -b %PROJECT_NAME% -clean
+exit /b 0
+
+
+:Init
+set "ERRORS="
+set "LASTERROR=0"
+exit /b 0 {ERRORS} {LASTERROR}
 
 
 :IsSharp <Project>
@@ -186,12 +223,7 @@ del /F /Q KlocworkReport.txt 2>nul
 del /F /Q KlocworkReport.csv 2>nul
 rem Check for Klocwork Flag
 call :IsTrue Klocwork || exit /b 0
-rem Clean Existing Caches
-rd /S /Q .kwps 2>nul
-rd /S /Q .kwlp 2>nul
-echo [INFO] Retrieving Klocwork Cache...
-xcopy "%KLOCWORK_CACHE%\%JOB_NAME%\.kwps" ".kwps\" /I /H /Q /E /Y >nul 2>&1
-xcopy "%KLOCWORK_CACHE%\%JOB_NAME%\.kwlp" ".kwlp\" /I /H /Q /E /Y >nul 2>&1
+call :KlocworkCacheGet
 rem Create Local Klocwork Project
 echo [INFO] Connecting to Klocwork Server...
 kwcheck create --url http://ipklocworkdb:80/FrontEnd_VC || kwcheck sync
@@ -207,19 +239,37 @@ call :IsSharp %4 || ( call :Build %1 %2 %3 NO null kwinject || exit /b 1 )
 rem Run Klocwork Analysis, and Report Generators
 echo [INFO] Running Klocwork Analysis...
 kwcheck run -y -b kwinject.out || exit /b 2
+echo [INFO] Generating Klocwork Reports...
 kwcheck list -y -F xml --report KlocworkReport.xml || exit /b 3
 kwcheck list -y -F detailed --report KlocworkReport.txt || exit /b 4
 kwcheck list -y -F scriptable --report KlocworkReport.csv || exit /b 5
 echo [INFO] Klocwork Analysis succeeded
-echo [INFO] Storing Klocwork Cache...
-rd /S /Q "%KLOCWORK_CACHE%\%JOB_NAME%" 2>nul
-xcopy ".kwps" "%KLOCWORK_CACHE%\%JOB_NAME%\.kwps\" /I /H /Q /E /Y >nul 2>&1
-xcopy ".kwlp" "%KLOCWORK_CACHE%\%JOB_NAME%\.kwlp\" /I /H /Q /E /Y >nul 2>&1
+echo [INFO] Klocwork Sync...
 kwcheck sync
+call :KlocworkCachePut
+exit /b 0
+
+
+:KlocworkCacheGet {MSBUILD_CACHE} {JOB_NAME} {VCCACHE} ~UI/IO
+rem Remove Existing Caches
+rd /S /Q .kwps 2>nul
+rd /S /Q .kwlp 2>nul
+call :IsTrue VCCACHE || exit /b
+echo [INFO] Retrieving Klocwork Cache...
+pushd "%KLOCWORK_CACHE%" && xcopy "%JOB_NAME%\.kwps" "%CD%\.kwps\" /I /H /Q /E /Y >nul 2>&1 & xcopy "%JOB_NAME%\.kwlp" "%CD%\.kwlp\" /I /H /Q /E /Y >nul 2>&1 & popd
+exit /b
+
+
+:KlocworkCachePut {KLOCWORK_CACHE} {JOB_NAME} {VCCACHE} ~UI/IO
+rem Remove Existing Cache
+pushd "%KLOCWORK_CACHE%" && rd /S /Q "%JOB_NAME%" 2>nul & popd
+call :IsTrue VCCACHE || exit /b
+echo [INFO] Storing Klocwork Cache...
+pushd "%KLOCWORK_CACHE%" && xcopy "%CD%\.kwps" "%JOB_NAME%\.kwps\" /I /H /Q /E /Y >nul 2>&1 & xcopy "%CD%\.kwlp" "%JOB_NAME%\.kwlp\" /I /H /Q /E /Y >nul 2>&1 & popd
 rem Remove Local Caches
 rd /S /Q .kwps 2>nul
 rd /S /Q .kwlp 2>nul
-exit /b %ErrorLevel%
+exit /b
 
 
 :KlocworkImport <Source> [Configurations] ~IO/UI
@@ -262,16 +312,20 @@ exit /b 0
 rem devenv /Build "%~1" /useenv
 
 
-:MSBuild <Configuration> <Version> <Verbosity> <Code Analysis> <Rule Set> [Klocwork] ~IO/UI {INCLUDE} {LIB} {LIBPATH} {WORKSPACE} {PROJECT} {PROJECT_NAME}
+:MSBuild <Configuration> <Version> <Verbosity> <Code Analysis> <Rule Set> [Klocwork|HPFortify] ~IO/UI {INCLUDE} {LIB} {LIBPATH} {WORKSPACE} {PROJECT} {PROJECT_NAME} {JOB_NAME} {SCM} {PROJECT_EXT} {OVERRIDE} {KLOCWORK}
 setlocal
+call :MSBuildCacheGet %1
 rem HACK: To fix the issue with deprecated regsvr32 post/custom build parameters.
 md %1 2>nul
 @echo off > "%~1\regsvr32.trg"
 rem Specify Special Version Specific Options
 set "ToolsVersion="
-if "%~2"=="120" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:12.0 /flp:LogFile="MSBuild%~1.log""
-if "%~2"=="110" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:11.0 /flp:LogFile="MSBuild%~1.log""
+if "%~2"=="120" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:12.0"
+if "%~2"=="110" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:11.0"
+if defined ToolsVersion call :Defined %6 && call :IsTrue HPFORTIFY || set "ToolsVersion=%ToolsVersion% /flp:LogFile="MSBuild%~1%6.log""
 if "%~2"=="100" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:4.0 /p:TargetFrameworkVersion=v3.5"
+if "%~2"=="100" if /i "%PROJECT_EXT%"==".csproj" set "ToolsVersion=/m /ignore:.sln /nr:false /tv:3.5 /p:TargetFrameworkVersion=v3.5"
+if "%~2"=="100" if /i "%OVERRIDE%"=="NO" set "ToolsVersion=/m /ignore:.sln /nr:false"
 if "%~2"=="90"  set "ToolsVersion="
 rem Enable Debug Build Code Analysis but not on Klocwork Debug Builds
 set "CodeAna="
@@ -279,18 +333,37 @@ if "%~2"=="120" if /i "%~1"=="Debug" set "CodeAna=/p:RunCodeAnalysis=true /p:Cod
 if /i not "%~4"=="YES" set "CodeAna=/p:RunCodeAnalysis=false"
 call :Defined %6 && set "CodeAna=/p:RunCodeAnalysis=false"
 set "PROJ=%PROJECT%"
-call :Download "http://scm.url.com/svn/jenkins/tools/VCBuild.proj" "%WORKSPACE%\%PROJECT_NAME%.proj" && set "PROJ=%PROJECT_NAME%.proj"
+call :Download "%SCM%jenkins/tools/VCBuild.proj" "%WORKSPACE%\%PROJECT_NAME%.proj" && set "PROJ=%PROJECT_NAME%.proj"
 rem Build the Command
-set "Command=MSBuild %PROJ% %ToolsVersion% /p:PlatformToolset=v%~2 /p:Configuration=%~1 /p:OutDir=%~1\ /p:IntDir=%~1\ /p:OutputPath=%~1\ /p:IncludePath="%INCLUDE%" /p:LibraryPath="%LIB%" /p:ReferencePath="%LIBPATH%" /p:PreBuildEvent="" /p:PostBuildEvent="" /p:CustomBuildStep="" /p:ImportLibrary="$(OutDir)$(TargetName).lib" /p:OutputFile="$(OutDir)$(TargetName)$(TargetExt)" /p:TypeLibraryName="$(OutDir)$(TargetName).tlb" /p:ProgramDatabaseFile="$(OutDir)$(TargetName).pdb" %CodeAna% /v:%~3"
+::set "Command=MSBuild %PROJ% %ToolsVersion% /p:PlatformToolset=v%~2 /p:Configuration=%~1 /p:OutDir=%~1\ /p:IntDir=%~1\ /p:OutputPath=%~1\ /p:IncludePath="%INCLUDE%" /p:LibraryPath="%LIB%" /p:ReferencePath="%LIBPATH%" /p:PreBuildEvent="" /p:PostBuildEvent="" /p:CustomBuildStep="" /p:ImportLibrary="$(OutDir)$(TargetName).lib" /p:OutputFile="$(OutDir)$(TargetName)$(TargetExt)" /p:TypeLibraryName="$(OutDir)$(TargetName).tlb" /p:ProgramDatabaseFile="$(OutDir)$(TargetName).pdb" %CodeAna% /v:%~3"
+set "Command=MSBuild %PROJ% %ToolsVersion% /p:PlatformToolset=v%~2 /p:Configuration=%~1 /p:OutDir=%~1\ /p:IntDir=%~1\ /p:OutputPath=%~1\ %CodeAna% /v:%~3"
 rem Update the Command for Klocwork
-call :Defined %6 && set "Command=%Command:"=\"% /t:Rebuild"
+call :Defined %6 && call :IsTrue KLOCWORK && set "Command=%Command:"=\"% /t:Rebuild"
+call :Defined %6 && call :IsTrue HPFORTIFY && set "Command=%Command% /t:Rebuild"
 rem Execute the Command
 @echo on
 %~6 %Command%
 @echo off
 set "ErrorCode=%ErrorLevel%"
 del /F /Q "%WORKSPACE%\%PROJECT_NAME%.proj" 2>nul
+call :MSBuildCachePut %1
 endlocal & exit /b %ErrorCode%
+
+
+:MSBuildCacheGet <Configuration> {MSBUILD_CACHE} {JOB_NAME} {VCCACHE} ~UI/IO
+rd /S /Q "%~1" 2>nul
+call :IsTrue VCCACHE || exit /b
+echo [INFO] Retrieving MSBuild Cache...
+pushd "%MSBUILD_CACHE%" && xcopy "%JOB_NAME%\%~1" "%CD%\%~1\" /I /H /Q /E /Y >nul 2>&1 & popd
+exit /b
+
+
+:MSBuildCachePut <Configuration> {MSBUILD_CACHE} {JOB_NAME} {VCCACHE} ~UI/IO
+pushd "%MSBUILD_CACHE%" && rd /S /Q "%JOB_NAME%\%~1" 2>nul & popd
+call :IsTrue VCCACHE || exit /b
+echo [INFO] Storing MSBuild Cache...
+pushd "%MSBUILD_CACHE%" && xcopy "%CD%\%~1" "%JOB_NAME%\%~1\" /I /H /Q /E /Y >nul 2>&1 & popd
+exit /b
 
 
 :ParseBranch <tag branch> {BRANCH} ~UI
@@ -325,7 +398,7 @@ echo [INFO] CACHE = %CACHE%
 echo [INFO] PIPELINE = %PIPELINE%
 if /i "%REPOSITORY:\=%"=="%CACHE:\=%%PIPELINE:\=%" exit /b 0
 echo [ERROR] REPOSITORY formatting may be incorrect
-exit /b 1 {CACHE} {PIPELINE}
+exit /b 1 : {CACHE} {PIPELINE}
 
 
 :Registration {REGISTER}
@@ -348,7 +421,7 @@ if not defined TlbImp echo [WARNING] Unable to find TlbImp
 if not defined TlbExp echo [WARNING] Unable to find TlbExp
 if not defined RegAsm echo [WARNING] Unable to find RegAsm
 if not defined RegSvcs echo [WARNING] Unable to find RegSvcs
-exit /b 0 {RegTLib} {TlbImp} {RegAsm} {RegSvcs} {RegSvr32}
+exit /b 0 : {RegTLib} {TlbImp} {RegAsm} {RegSvcs} {RegSvr32}
 
 
 :ReplaceLabels {LABEL_*}
@@ -357,6 +430,8 @@ call :Define %~1 %%%~1:VS2012=110%%
 call :Define %~1 %%%~1:VS2010=100%%
 call :Define %~1 %%%~1:VS2008=90%%
 call :Define %~1 %%%~1:KLOCWORK=YES%%
+call :Define %~1 %%%~1:PVSSTUDIO=YES%%
+call :Define %~1 %%%~1:HPFORTIFY=YES%%
 exit /b 0
 
 
@@ -383,6 +458,14 @@ call :Defined %3 && set "Command=%Command:"=\"% /rebuild"
 endlocal & exit /b %ErrorLevel%
 
 
+::"Debug" "Release" "ReleaseWithDebug" "Release With Debug"
+:VCClean <Configuration> {PROJECT}
+echo [INFO] Removing Generated Files...
+for %%A in ("%~1" "Bin" "Obj" "TestResult" "BuildLog" "Build" "x64" "ipch" "Express" "publish" "AppPackages" "StyleCop" "Generated_Code" "_UpgradeReport_Files" "$RECYCLE.BIN") do if exist "%%~A" 2>nul rd /Q "%%~A"
+for %%A in ("*.exe" "*.dll" "*.ocx" "*.lib" "*.suo" "*.user" "*.sln.docstates" "*_i.h" "*_i.c" "*_p.c" "*.ilk" "*.meta" "*.obj" "*.pch" "*.pdb" "*.pgc" "*.pgd" "*.rsp" "*.sbr" "*.tlb" "*.tli" "*.tlh" "*.tmp" "*.tmp_proj" "*.vspscc" "*.vssscc" ".builds" "*.pidb" "*.scc" "*.aps" "*.ncb" "*.opensdf" "*.sdf" "*.cachefile" "*.psess" "*.vsp" "*.vspx" "*.gpState" "*.ReSharper" "*.dotCover" "*.Publish.xml" "*.pubxml" "*.cache" "*.dbmdl" "*.Publish.xml" "*.pfx" "*.publishsettings" "Thumbs.db" "ehthumbs.db" "Desktop.ini" ".DS_Store") do 2>nul del /F /Q /S "%%~A"
+exit /b 0
+
+
 :: initialize the visual studio environment
 :VCVars <Version> <Architecture> ~IO/UI
 call :Defined "%~1" || exit /b 1
@@ -394,7 +477,7 @@ if "%~1"=="110" call "%ProgramFiles32%\Microsoft Visual Studio 11.0\VC\vcvarsall
 if "%~1"=="120" call "%ProgramFiles32%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %~2 && exit /b 0
 echo [ERROR] Not found or unknown version of Visual Studio specified. Version=%~1
 echo [INFO] Supported versions include 90, 100, 110, and 120, but not all nodes support all versions.
-exit /b 3 {ProgramFiles32}
+exit /b 3 : {ProgramFiles32}
 
 
 :::::::::::::::::::::::::::::::::::FRAMEWORK:::::::::::::::::::::::::DO:NOT:EDIT
@@ -411,6 +494,20 @@ setlocal
 call :Define List "%%%~1%%" || ( endlocal & exit /b 1 )
 for %%A in ("%List:;=" "%") do if not exist "%%~fA\" echo [INFO] "%%~A" not found.
 endlocal & exit /b 0
+
+
+:CopyFirst <File> <Destination> {PATH} {LIBPATH} {LIB}
+setlocal
+set "_=%~dp1;%LIBPATH%;%LIB%;%PATH%"
+call :TidyList _ >nul
+call :CopyFirst_ %1 %2
+endlocal & exit /b %ErrorLevel%
+
+
+:CopyFirst_ <File> <Destination> {_}
+md "%~2" 2>nul || exit /b 1
+if defined _ for %%A in ("%_:;=" "%") do if exist "%%~A\%~nx1" ( echo f|xcopy /y /r "%%~A\%~nx1" "%~2\%~nx1" >nul 2>&1 & exit /b )
+exit /b 2
 
 
 :DeferError <Command> [Params]
@@ -465,15 +562,15 @@ endlocal & exit /b %ErrorLevel%
 
 
 :IsTrue <Var>
-call :EqualsTrue %%%~1%% && exit /b 0
-exit /b %ErrorLevel%
+call :EqualsTrue "%%%~1%%" || exit /b
+exit /b 0
 
 
-:LogErrors {ErrorLevel} {Errors} {LastError} ~UI
+:LogDeferredErrors {Errors} {LastError} ~UI
 echo.
-if not defined Errors if %ErrorLevel% equ 0 echo [INFO] No Errors Detected
-if defined Errors echo [ERROR] Errors Encountered = %Errors%%ErrorLevel%
-exit /b %ErrorLevel%
+if not defined Errors echo [INFO] No Errors Detected
+if defined Errors echo [ERROR] Errors Encountered = %Errors%
+exit /b %LastError%
 
 
 :!
@@ -525,15 +622,17 @@ exit /b 1
 
 
 :RegisterASM <File> {RegAsm} ~UI/IO
-call :RegisterASMTLB && exit /b
+::call :RegisterASMTLB %1 && exit /b
 "%RegAsm%" /nologo /silent "%~f1" /codebase 2>nul && call :Registered %1 ASM
 exit /b
 
 
 :RegisterASMTLB <File> {RegAsm} ~UI/IO
-if exist "%~dpn1.tlb" exit /b 1
-"%RegAsm%" /nologo /silent "%~f1" /tlb:"%~dpn1.tlb" /codebase 2>nul && call :Registered %1 "REGISTERED ASM + GENERATED TLB"
+if exist "WinIntel\Lib\%~n1.tlb" exit /b 1
+md WinIntel\Lib 2>nul
+"%RegAsm%" /nologo /silent "%~f1" /tlb:"WinIntel\Lib\%~n1.tlb" /codebase 2>nul && call :Registered %1 "REGISTERED ASM + GENERATED TLB"
 exit /b
+::&& call :RegisterTLB "WinIntel\Lib\%~n1.tlb"
 
 
 :RegisterSVR <File> {RegSvr32} ~UI/IO
@@ -548,20 +647,32 @@ exit /b
 
 :RegisterDLL <File> {RegAsm} {RegSvcs} {RegSvr32} {TlbExp} ~UI/IO
 call :RegisterSVR %1 || call :RegisterASM %1 || call :RegisterSVCS %1 || call :Registered %1 SKIPPED
-call :RegisterTLB "%~dpn1.tlb" & exit /b %ErrorLevel%
-:: TODO Extract tlbexp into its own explicit workflow as to not interfere with the reg* result
-rem if not exist "%~dpn1.tlb" "%TlbExp%" "%~f1" /out:"%~dpn1.tlb" /silent 2>nul && echo [EXPORTED TLB] "%~f1"
+exit /b
 
 
-:RegisterTLB <File> {RegTLib} {TlbImp} ~UI/IO
+:RegisterTLB <File> {RegTLib} ~UI/IO
+call :RegisterTLBImport %1
 "%RegTLib%" "%~1" 2>nul && call :Registered %1 TLB || call :Registered %1 SKIPPED
-call :RegisterTLBImport %1 & exit /b %ErrorLevel%
-:: TODO Extract tlbimp into its own explicit workflow as to not interfere with the regtlib result
-rem if not exist "%~dpn1.dll" "%TlbImp%" "%~1" /out:"%~dpn1.dll" /silent 2>nul && echo [IMPORTED TLB + GENERATED DLL] "%~f1"
+exit /b
+
+
+:RegisterTLBExport <File> {TlbExp} ~UI/IO
+if exist "WinIntel\Lib\%~n1.tlb" exit /b 1
+md WinIntel\Lib 2>nul
+"%TlbExp%" "%~f1" /out:"WinIntel\Lib\%~n1.tlb" /silent 2>nul && echo [EXPORTED TLB] "%~f1"
+exit /b
 
 
 :RegisterTLBImport <File> {TlbImp} ~UI/IO
+::call :RegisterTLBImportDLL %1 && exit /b
 "%TlbImp%" "%~1" /silent 2>nul && echo [IMPORTED TLB] "%~f1"
+exit /b
+
+
+:RegisterTLBImportDLL <File> {TlbImp} ~UI/IO
+if exist "WinIntel\%~n1.dll" exit /b 1
+md WinIntel 2>nul
+"%TlbImp%" "%~1" /out:"WinIntel\%~n1.dll" /silent 2>nul && echo [IMPORTED TLB + GENERATED DLL] "%~f1"
 exit /b
 
 
@@ -595,7 +706,7 @@ call :Define "%~1" ";%%%~1:\\=\%%"
 call :Define "%~1" "%%%~1:;;=;%%"
 call :Define "%~1" "%%%~1:;\=;\\%%"
 call :Define "%~1" "%%%~1:~1%%"
-call :CheckList %1
+::call :CheckList %1
 exit /b
 
 
@@ -617,7 +728,7 @@ exit /b
 
 :UnRegisterDLL <File> {RegAsm} {RegSvcs} {RegSvr32} ~UI/IO
 "%RegSvr32%" /u /s "%~f1" && echo [DEREGISTERED SVR] "%~f1" || "%RegAsm%" /unregister /nologo /silent "%~f1" 2>nul && echo [DEREGISTERED ASM] "%~f1" || "%RegSvcs%" /u /quiet "%~f1" 2>nul && echo [DEREGISTERED SVCS] "%~f1" || echo [DEREGISTERED SKIPPED] "%~f1"
-if exist "%~dpn1.tlb" call :UnRegisterTLB "%~dpn1.tlb"
+if exist "WinIntel\Lib\%~n1.tlb" call :UnRegisterTLB "WinIntel\Lib\%~n1.tlb"
 exit /b
 
 
