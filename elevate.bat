6e01fcaa29043635ce93235faefde5ee5019c7dd
David Ruhmann <davidruhmann@gmail.com>
2020-07-11 19:11:34 -0400

    add note about specifically elevating cmd.exe

--- elevate.bat
+++ elevate.bat
@@ -9,14 +9,16 @@ PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "$0 = '%1'; $cl = '
 endlocal
 exit /b
 :ElevateMe
+:: ElevateMe does not work from inside cmd.exe due to batch files running in the same process, causing the grand parent to be elevated.  Can work for cmd and powershell, or for everything bug cmd. if ( $parent.ExecutablePath -like '*cmd.exe' ) { $gparent = $parent } else { ... }
 setlocal DisableDelayedExpansion
-PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "gwmi -Class Win32_Process -Filter ('ProcessId = ' + $pid) | foreach { $parent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $_.ParentProcessId)}; if ( $parent.ExecutablePath -like '*cmd.exe' ) { $gparent = $parent } else { $gparent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $parent.ParentProcessId) }; if ( $gparent.CommandLine.Length -gt $gparent.ExecutablePath.Length ) { $args = ($gparent.CommandLine.Trim().Trim([char]0x22)).Substring($gparent.ExecutablePath.Length) }; if ( $var -eq $null ) { $args = ' ' }; start $gparent.ExecutablePath -Verb runas -ArgumentList $args -WorkingDirectory (cvpa .); exit(!$?);"
+PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "gwmi -Class Win32_Process -Filter ('ProcessId = ' + $pid) | foreach { $parent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $_.ParentProcessId)}; $gparent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $parent.ParentProcessId); if ( $gparent.CommandLine.Length -gt $gparent.ExecutablePath.Length ) { $args = ($gparent.CommandLine.Trim().Trim([char]0x22)).Substring($gparent.ExecutablePath.Length) }; if ( $var -eq $null ) { $args = ' ' }; start $gparent.ExecutablePath -Verb runas -ArgumentList $args -WorkingDirectory (cvpa .); exit(!$?);"
+:: TODO if failed, Attempt elevate with execution path
 endlocal
 exit /b
 :Error
-:: TODO display usage information.
 echo.Failed to elevate with Administrator privileges
 echo.See "%Temp%\%~n0.log" for details.
+echo.If attempting to elevate cmd.exe, use: 'elevate cmd'
 exit /b
 :Futile
 echo.Session already has Administrator privileges
