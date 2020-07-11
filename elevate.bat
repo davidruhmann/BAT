46326c1b07c81cd32f0af2cb26d86b700e76e6a1
David Ruhmann <davidruhmann@gmail.com>
2020-07-11 18:23:44 -0400

    fix ElevateMe for Win10 and windows terminal

--- elevate.bat
+++ elevate.bat
@@ -2,7 +2,7 @@
 if not "%~1"=="" call :Elevate %* 2>"%Temp%\%~n0.log" && exit /b || ( call :Error & exit /b )
 call :IsAdmin && ( call :Futile & exit /b )
 call :ElevateMe 2>"%Temp%\%~n0.log" || ( call :Error & exit /b )
-exit
+exit /b
 :Elevate <Executable> [Arguments]
 setlocal DisableDelayedExpansion
 PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "$0 = '%1'; $cl = '%*'; $ar = $cl.Substring($0.Length); if (![bool]$ar) {$ar = ' '}; start $0.Trim([char]0x22) -Verb runas -ArgumentList $ar -WorkingDirectory (cvpa .); exit(!$?);"
@@ -10,10 +10,11 @@ endlocal
 exit /b
 :ElevateMe
 setlocal DisableDelayedExpansion
-PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "gwmi -Class Win32_Process -Filter ('ProcessId = ' + $pid) | foreach { $parent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $_.ParentProcessId)}; $gparent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $parent.ParentProcessId); $args = $gparent.CommandLine.Trim().Trim([char]0x22).Substring($gparent.ExecutablePath.Length) + ' '; start $gparent.ExecutablePath -Verb runas -ArgumentList $args -WorkingDirectory (cvpa .); exit(!$?);"
+PowerShell -NoProfile -ExecutionPolicy RemoteSigned -Command "gwmi -Class Win32_Process -Filter ('ProcessId = ' + $pid) | foreach { $parent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $_.ParentProcessId)}; if ( $parent.ExecutablePath -like '*cmd.exe' ) { $gparent = $parent } else { $gparent = gwmi -Class Win32_Process -Filter ('ProcessId = ' + $parent.ParentProcessId) }; if ( $gparent.CommandLine.Length -gt $gparent.ExecutablePath.Length ) { $args = ($gparent.CommandLine.Trim().Trim([char]0x22)).Substring($gparent.ExecutablePath.Length) }; if ( $var -eq $null ) { $args = ' ' }; start $gparent.ExecutablePath -Verb runas -ArgumentList $args -WorkingDirectory (cvpa .); exit(!$?);"
 endlocal
 exit /b
 :Error
+:: TODO display usage information.
 echo.Failed to elevate with Administrator privileges
 echo.See "%Temp%\%~n0.log" for details.
 exit /b
